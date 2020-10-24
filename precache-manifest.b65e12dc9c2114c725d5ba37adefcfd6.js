f20b43e118878f14f420d1cd5ed5890846da02fe
Kirill Kulakov <kirill578@gmail.com>
2020-10-23 21:34:17 -0500

    Updates

---
+++ precache-manifest.b65e12dc9c2114c725d5ba37adefcfd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10179825c7777e82fea887630d2ebadc",
+    "url": "/muscle_spelling/index.html"
+  },
+  {
+    "revision": "dbaa443970038f91c353",
+    "url": "/muscle_spelling/static/css/2.2a61b6f0.chunk.css"
+  },
+  {
+    "revision": "facb745a9cdec5b4d897",
+    "url": "/muscle_spelling/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dbaa443970038f91c353",
+    "url": "/muscle_spelling/static/js/2.455fffa8.chunk.js"
+  },
+  {
+    "revision": "84baf43984eed097aab2",
+    "url": "/muscle_spelling/static/js/3.d8171a90.chunk.js"
+  },
+  {
+    "revision": "facb745a9cdec5b4d897",
+    "url": "/muscle_spelling/static/js/main.51670d00.chunk.js"
+  },
+  {
+    "revision": "a8aa7154421d3a1c81c2",
+    "url": "/muscle_spelling/static/js/runtime-main.9a8843bb.js"
+  },
+  {
+    "revision": "23730a41633e1dc7dda55bb160d2ede0",
+    "url": "/muscle_spelling/static/media/fail.23730a41.mp3"
+  },
+  {
+    "revision": "a8e58690a8d6f64dcdf72824e41e8b1a",
+    "url": "/muscle_spelling/static/media/success.a8e58690.mp3"
+  }
+]);
